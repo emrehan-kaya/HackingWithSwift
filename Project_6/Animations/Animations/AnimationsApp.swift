1c842602ce05ee08d8ab0d85d7b33b86cc3a434e
Emrehan Kaya <86688101+emrehan-kaya@users.noreply.github.com>
2024-09-05 22:23:50 +0300

    Add files via upload

---
+++ Project_6/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Emrehan Kaya on 5.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
