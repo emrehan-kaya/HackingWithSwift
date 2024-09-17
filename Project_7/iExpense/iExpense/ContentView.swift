b7e3ea3cca895d1b5d708da745f972419bf0cbb7
Emrehan Kaya <86688101+emrehan-kaya@users.noreply.github.com>
2024-09-17 20:37:01 +0300

    Add files via upload

---
+++ Project_7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,80 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Emrehan Kaya on 16.09.2024.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey:"Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self,from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
+
+struct ContentView: View {
+
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                                .foregroundStyle(item.amount < 11 ? .green : false || item.amount > 1000 ? .red : .blue)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        
+                        Text(item.amount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(item.amount < 11 ? .green : false || item.amount > 1000 ? .red : .blue)
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                   showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: Expenses())
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project_7/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Emrehan Kaya on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
